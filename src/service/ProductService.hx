ed8a9e2ba7819c2e51b0eb975d86c51b081bb2ec
Mandrak-Kimigo <108131079+Mandrak-Kimigo@users.noreply.github.com>
2023-05-24 15:07:26 +0200

    Bug calcul stock
    Apres duplication d'un catalogue avec produits ou lors d'un import de produits dans catalogue sans distri bug d'affichage du stock (div / 0)

--- src/service/ProductService.hx
+++ src/service/ProductService.hx
@@ -107,8 +107,12 @@ class ProductService{
 			var stock = f.getElement("stock");
 			stock.label = "Stock (par distribution)";				 
 			if(product.stock!=null){
-				stock.value = Math.floor( product.stock / product.catalog.getDistribs(false).length );
-			}		
+				distLeft = product.catalog.getDistribs(false).length;
+				if (distLeft > 0) {
+					stock.value = Math.floor( product.stock / distLeft );
+			} else {
+				stock.value = product.stock;
+			}
 		}
 
 		var group = product.catalog.group;
