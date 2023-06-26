b0838630dfe5396554b3c5f23f8e6a5ea1d79f22
François Barbut <francois@alilo.fr>
2023-06-26 14:26:24 +0200

    rights on questionnaire

--- src/controller/Main.hx
+++ src/controller/Main.hx
@@ -138,7 +138,6 @@ class Main extends Controller {
 						case "bye" : "Je ne souhaite plus utiliser CAMAP, ni Cagette.net";
 						default : "???";
 					};
-	
 					App.current.session.addMessage("<h4>Reprise de CAMAP par l'interAMAP 44</h4>Votre réponse au questionnaire : <b>"+choice+"</b>, le "+view.hDate(g.questDate)+" par "+g.questUser.getName());
 				}else{
 					App.current.session.addMessage("<h4>Reprise de CAMAP par l'interAMAP 44</h4>En tant qu'administrateur de cette AMAP, <b>si vous désirez continuer à utiliser l'application sans pertes de données lors de la migration</b>,<br/>merci de bien vouloir remplir ce questionnaire avant le 3 Juillet 2023 : <a href='/questionnaire' class='btn btn-default'>Questionnaire</a>");
@@ -346,47 +345,53 @@ class Main extends Controller {
 	@tpl('form.mtt')
 	public function doQuestionnaire(){
 
-		view.title = "Questionnaire pour connaître vos intentions quant à la reprise de CAMAP par l'InterAMAP 44";
+		var ug = app.user.getUserGroup(app.getCurrentGroup());
+		if(ug.getRights().length>0 || app.user.isAdmin()){
 
-		view.text = "<div class='alert alert-warning'>
-		<p>Ce questionnaire est réservé aux administrateurs d'AMAP</p>
-		<p><a href='https://amap44.org' target='_blank'>L'InterAMAP 44</a> a été choisie par Alilo pour reprendre l'hébergement et la maintenance du logiciel CAMAP.</p>
-		<p>Nous proposons aux AMAP qui le souhaitent de basculer sur leur serveur CAMAP <b>la semaine du 3 juillet 2023</b></p>
-		<p>
-		Si celà vous intéresse, nous vous invitons vivement à étudier leur offre d'hébergement : <a href='https://www.amap44.org/camap/' class='btn btn-default' target='_blank'>Conditions hébergement InterAMAP 44</a>.
-		</p>		
-		</div>
-		Afin de nous faire connaître vos intentions, merci de bien vouloir remplir ce formulaire avant le 3 juillet 2023 :";
+			view.title = "Questionnaire pour connaître vos intentions quant à la reprise de CAMAP par l'InterAMAP 44";
 
-		var f = new sugoi.form.Form("quest");
-		f.addElement(new sugoi.form.elements.Html("user",app.user.getName(),"Votre nom"));
-		f.addElement(new sugoi.form.elements.Html("amap",app.getCurrentGroup().name,"Votre AMAP"));
+			view.text = "<div class='alert alert-warning'>
+			<p>Ce questionnaire est réservé aux administrateurs d'AMAP</p>
+			<p><a href='https://amap44.org' target='_blank'>L'InterAMAP 44</a> a été choisie par Alilo pour reprendre l'hébergement et la maintenance du logiciel CAMAP.</p>
+			<p>Nous proposons aux AMAP qui le souhaitent de basculer sur leur serveur CAMAP <b>la semaine du 3 juillet 2023</b></p>
+			<p>
+			Si celà vous intéresse, nous vous invitons vivement à étudier leur offre d'hébergement : <a href='https://www.amap44.org/camap/' class='btn btn-default' target='_blank'>Conditions hébergement InterAMAP 44</a>.
+			</p>		
+			</div>
+			Afin de nous faire connaître vos intentions, merci de bien vouloir remplir ce formulaire avant le 3 juillet 2023 :";
+	
+			var f = new sugoi.form.Form("quest");
+			f.addElement(new sugoi.form.elements.Html("user",app.user.getName(),"Votre nom"));
+			f.addElement(new sugoi.form.elements.Html("amap",app.getCurrentGroup().name,"Votre AMAP"));
+	
+			var data = [
+				{label:"Je souhaite basculer sur le serveur CAMAP de l'InterAMAP 44 la semaine du 3 juillet 2023. Je donne mon accord pour que les données de mon AMAP soient transférées à l'InterAMAP 44 afin de pouvoir continuer à utiliser le logiciel sans perte de données.",value:"move"},
+				{label:"Je souhaite rester sur ce serveur. J'ai compris que le service s'arrêtera le 30 Août 2023 sans reprise de données possible.",value:"stay"},
+				{label:"Je souhaite revenir sur Cagette.net pour gérer des commandes en \"mode marché\".",value:"cagette"},
+				{label:"Je ne souhaite plus utiliser CAMAP, ni Cagette.net",value:"bye"},
+			];
+	
+			f.addElement(new RadioGroup("choice","Choix pour mon AMAP :",data));
+	
+			if(f.isValid()){
+	
+				var g = App.current.getCurrentGroup();
+				g.lock();
+	
+				g.questUser = app.user;
+				g.questDate = Date.now();
+				g.questAnswer = f.getValueOf("choice");
+				g.update();
+	
+				throw Ok("/home","Merci pour votre réponse.");
+	
+			}
+	
+	
+			view.form = f;
+		}	
 
-		var data = [
-			{label:"Je souhaite basculer sur le serveur CAMAP de l'InterAMAP 44 la semaine du 3 juillet 2023. Je donne mon accord pour que les données de mon AMAP soient transférées à l'InterAMAP 44 afin de pouvoir continuer à utiliser le logiciel sans perte de données.",value:"move"},
-			{label:"Je souhaite rester sur ce serveur. J'ai compris que le service s'arrêtera le 30 Août 2023 sans reprise de données possible.",value:"stay"},
-			{label:"Je souhaite revenir sur Cagette.net pour gérer des commandes en \"mode marché\".",value:"cagette"},
-			{label:"Je ne souhaite plus utiliser CAMAP, ni Cagette.net",value:"bye"},
-		];
-
-		f.addElement(new RadioGroup("choice","Choix pour mon AMAP :",data));
-
-		if(f.isValid()){
-
-			var g = App.current.getCurrentGroup();
-			g.lock();
-
-			g.questUser = app.user;
-			g.questDate = Date.now();
-			g.questAnswer = f.getValueOf("choice");
-			g.update();
-
-			throw Ok("/home","Merci pour votre réponse.");
-
-		}
-
-
-		view.form = f;
+		
 
 	}
 
