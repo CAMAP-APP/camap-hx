6895db44c1321189f2da116aedca2aaa201994d4
Mandrak-Kimigo <108131079+Mandrak-Kimigo@users.noreply.github.com>
2023-07-03 15:26:23 +0200

    CSS work

--- backend/buildSass.sh
+++ backend/buildSass.sh
@@ -2,6 +2,6 @@
 
 for dir in ../www/theme/*/; do
     echo "build "$dir"css/style.min.css"
-    sass $dir/css/bootswatch.scss $dir/css/style.css
+    sass $dir/css/style.scss $dir/css/style.css
     sass --style=compressed $dir/css/style.scss $dir/css/style.min.css
 done
