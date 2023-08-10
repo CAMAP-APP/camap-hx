964022b2f1e1e0991132d1b79476616c3e2efb90
Mandrak-Kimigo <108131079+Mandrak-Kimigo@users.noreply.github.com>
2023-08-10 23:28:48 +0200

    Gestion des Stocks
    function calculateStock

--- src/service/ProductService.hx
+++ src/service/ProductService.hx
@@ -157,9 +157,9 @@ class ProductService{
 	public static function calculateStock (catalog:db.Catalog, product:db.Product):Float {
 		if (catalog.hasStockManagement()) {
 			var now = Date.now();
-			var nextDistrib = db.Distribution.manager.search(( $orderEndDate > now && $catalogId==catalog.id), { limit:1});
+			var nextDistribs = db.Distribution.manager.search( ($orderEndDate > now && $catalogId==catalog.id),{orderBy:orderEndDate}).array();
 			var totOrdersQt : Float = 0;
-			var actualOrders = db.UserOrder.manager.search($product==product && $distributionId==nextDistrib.id, true);
+			var actualOrders = db.UserOrder.manager.search($product==product && $distributionId==nextDistrib[0].id, true);
 			for (actualOrder in actualOrders) {
 				totOrdersQt += actualOrder.quantity;
 			}
