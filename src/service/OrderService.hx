4c0de4187087ceec19e009062b0548f9bb6a99f6
Mandrak-Kimigo <108131079+Mandrak-Kimigo@users.noreply.github.com>
2023-08-17 14:02:30 +0200

    Gestion des Stocks
    debug multipesée

--- src/service/OrderService.hx
+++ src/service/OrderService.hx
@@ -133,7 +133,7 @@ class OrderService
 				for (actualOrder in actualOrders) {
 					totOrdersQt += actualOrder.quantity;
 					if (App.config.DEBUG){
-						var msg='Commandes précédentes: ' +totOrdersQt+ 'pour l\'utilisateur' +user.id;
+						var msg= '${DateTools.format(order.distribution.date,"%d/%m/%Y")} Commandes précédentes du: ' +totOrdersQt+ 'pour l\'utilisateur' +user.id;
 						App.current.session.addMessage (msg);
 					}
 				}
