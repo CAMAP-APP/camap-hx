bf245cdf7c2bb9037c7c8e3c537bd29dda62e12b
Mandrak-Kimigo <108131079+Mandrak-Kimigo@users.noreply.github.com>
2023-09-04 14:50:17 +0200

    Revert "Revert "Merge branch 'master' into master""
    This reverts commit 4e497b03d052acbe3ba57df6855a9c6ea37c3b0d.

--- src/controller/SubscriptionAdmin.hx
+++ src/controller/SubscriptionAdmin.hx
@@ -364,7 +364,11 @@ class SubscriptionAdmin extends controller.Controller
 	public function doBalanceTransfer( subscription : db.Subscription ) {
 
 		if ( !app.user.canManageContract( subscription.catalog ) ) throw Error( '/', t._('Access forbidden') );
-		if ( subscription.getBalance() <= 0 ) throw Error( '/contractAdmin/subscriptions/payments/' + subscription.id, 'Le solde doit être positif pour pouvoir le transférer sur une autre souscription.' );
+		/** Permettre le transfert de souscriptions négatives 
+		*/
+		/*
+		 if ( subscription.getBalance() <= 0 ) throw Error( '/contractAdmin/subscriptions/payments/' + subscription.id, 'Le solde doit être positif pour pouvoir le transférer sur une autre souscription.' );
+		*/
 		var subscriptionsChoices = SubscriptionService.getUserVendorNotClosedSubscriptions( subscription );
 		if ( subscriptionsChoices.length == 0  ) throw Error( '/contractAdmin/subscriptions/payments/' + subscription.id, 'Ce membre n\'a pas d\'autre souscription. Veuillez en créer une nouvelle avec le même producteur.' );
 
