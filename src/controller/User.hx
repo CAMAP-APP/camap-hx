9379d9a71c4ca6cdc4412b0276999855224d7265
Mandrak-Kimigo <108131079+Mandrak-Kimigo@users.noreply.github.com>
2023-09-18 10:44:53 +0200

    Invalid field access : balance

--- src/controller/User.hx
+++ src/controller/User.hx
@@ -336,10 +336,11 @@ class User extends Controller
 				}
 			}
 		}
-		var userGroup = db.UserGroup.get(app.user, group);
-		if (userGroup.balance < 0) throw Error ("/","Vous ne pouvez pas quitter ce groupe car votre solde est négatif.\nVeuillez contacter un responsable du groupe pour plus d'information.");
+
 		view.groupId = group.id;
 		if (app.user!=null) {
+			var userGroup = db.UserGroup.get(app.user, group);
+			if (userGroup.balance < 0) throw Error ("/","Vous ne pouvez pas quitter ce groupe car votre solde est négatif.\nVeuillez contacter un responsable du groupe pour plus d'information.");
 			view.userId = app.user.id;
 			view.controlKey = haxe.crypto.Sha1.encode(App.config.KEY+group.id+app.user.id);
 		}
