488a05ad1fd125aaaf28c1f7dd600e6eb7bdc29a
Samuel Bouchet <contact@samuel-bouchet.fr>
2024-05-16 08:04:11 +0200

    Fix isMemberOfGroup on group public page

--- src/controller/Group.hx
+++ src/controller/Group.hx
@@ -57,10 +57,8 @@ class Group extends controller.Controller
 		if ( app.user == null ) {
 			service.UserService.prepareLoginBoxOptions(view,group);
 		}	
-		// view.user = app.user;
-		// view.isMember = isMemberOfGroup;
-		view.user = null;
-		view.isMember = false;
+		view.user = app.user;
+		view.isMember = isMemberOfGroup;
 
 		// Documents
 		view.visibleGroupDocuments = group.getVisibleDocuments( isMemberOfGroup );
