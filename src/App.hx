3a7efaf9fa3feaa7e61bed4d9984ec23e1318180
Mandrak-Kimigo <amaury.cormier@gmail.com>
2024-11-07 19:53:36 +0100

    v2.0.4

--- src/App.hx
+++ src/App.hx
@@ -19,7 +19,7 @@ class App extends sugoi.BaseApp {
 	 * Version management
 	 * @doc https://github.com/fponticelli/thx.semver
 	 */ 
-	public static var VERSION = ([2,0,2]  : Version)/*.withPre(GitMacros.getGitShortSHA(), GitMacros.getGitCommitDate())*/;
+	public static var VERSION = ([2,0,4]  : Version)/*.withPre(GitMacros.getGitShortSHA(), GitMacros.getGitCommitDate())*/;
 	
 	public function new(){
 		super();
