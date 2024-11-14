1ec4d007b316582efa8892f2fef0e57792105d95
Mandrak-Kimigo <amaury.cormier@gmail.com>
2024-11-14 20:04:17 +0100

    MantisBT 223 the retour of the bug
    Ajout du check des absences sur la date de début modifiée

--- src/controller/SubscriptionAdmin.hx
+++ src/controller/SubscriptionAdmin.hx
@@ -287,7 +287,7 @@ class SubscriptionAdmin extends controller.Controller
 
 				for (id in absenceDistribIds){
 					var d = db.Distribution.manager.get(id,false);
-					if(d!=null && d.date.getTime()>endDate.getTime()){
+					if((d!=null && d.date.getTime()>endDate.getTime())|| (d!=null && d.date.getTime()<startDate.getTime())){
 						throw Error( '/contractAdmin/subscriptions/edit/' + subscription.id, "Vous ne pouvez pas sélectionner une absence postérieure à la date de fin de souscription. Vous devez d'abord modifier l'absence." );
 					}
 				}
