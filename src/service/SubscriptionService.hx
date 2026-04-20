d6c68d1a6bc2f18bee5a2bd2661fb0e440794959
Mandrak-Kimigo <amaury.cormier@gmail.com>
2026-04-20 07:36:32 +0200

    mantis #315

--- src/service/SubscriptionService.hx
+++ src/service/SubscriptionService.hx
@@ -310,7 +310,7 @@ class SubscriptionService {
 		// catalog must have the flag UsersCanOrder or user must have admin rights on catalog
 
 		// if (!subscription.catalog.hasOpenOrders() && (!app.user.isContractManager() || !app.user.isAdmin() || !app.user.isGroupManager()))
-		if (!subscription.catalog.hasOpenOrders() && !adminMode) {
+		if (!subscription.catalog.hasOpenOrders() && (!adminMode || !app.user.canManageContract(subscription.catalog)) {
 			throw new Error("Les souscriptions à ce catalogue sont fermées. Veuillez contacter le coordinateur du contrat.");
 		}
 
